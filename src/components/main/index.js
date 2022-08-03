2a1bf602d286fb13e39e78ed2277bb6198a3bdd1
yusufneeson <yusufneeson@gmail.com>
2022-08-03 14:47:06 +0700

    Added Main component

---
+++ src/components/main/index.js
@@ -0,0 +1,12 @@
+import Shorter from "../shorter";
+import style from "./main.module.css";
+
+function Main() {
+	return (
+		<section className={style.main}>
+			<Shorter />
+		</section>
+	);
+}
+
+export default Main;
