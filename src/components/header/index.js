162223ad7c2ccf9bae0c59a7283b51a3ee4f0c56
yusufneeson <yusufneeson@gmail.com>
2022-08-03 11:24:52 +0700

    header: added responsive mobile menu

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,15 +1,48 @@
+import { useEffect, useRef, useState } from "react";
 import Menu from "../menu";
 import style from "./header.module.css";
 
 function Header() {
+	const [isOpen, setIsOpen] = useState(false);
+	const ref = useRef();
+
+	useEffect(() => {
+		if (!isOpen) return;
+		const checkIfClickedOutside = (e) => {
+			if (isOpen && ref.current && !ref.current.contains(e.target)) {
+				setIsOpen(false);
+			}
+		};
+		document.addEventListener("mousedown", checkIfClickedOutside);
+		return () => {
+			document.removeEventListener("mousedown", checkIfClickedOutside);
+		};
+	}, [isOpen]);
+
 	return (
 		<header className={style.header}>
-			<nav className={style.navbar}>
+			<nav className={style.navbar} ref={ref}>
 				<a href="/" className={style.brand}>
 					<img src="/images/logo.svg" alt="Logo" />
 				</a>
 
-				<Menu />
+				{isOpen && (
+					<div className={style.mobile}>
+						<div className={style.container}>
+							<Menu style={style} />
+						</div>
+					</div>
+				)}
+
+				<Menu style={style} />
+				<div
+					onClick={() => setIsOpen(!isOpen)}
+					className={style.burger}
+				>
+					<div className={style.line}></div>
+					<div className={style.line}></div>
+					<div className={style.line}></div>
+				</div>
 			</nav>
 		</header>
 	);
