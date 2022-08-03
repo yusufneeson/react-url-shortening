8c25a83f672d104dbf6b0620564dc28216fa2fbe
yusufneeson <yusufneeson@gmail.com>
2022-08-03 10:49:43 +0700

    App.js: Call `Header` component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Header from "./components/header";
+
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<Header />
+		</div>
+	);
 }
 
 export default App;
